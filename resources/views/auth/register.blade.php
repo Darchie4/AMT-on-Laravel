bd86ba95ac7a92b6a5975e40c0ae933ee4043247
Mads W P <mads456dk@gmail.com>
2024-03-10 21:57:10 +0100

    Revert "Merge branch 'admin_lesson' into develop"
    This reverts commit d93a9fec8efc52e34d147ad5be41cecfde87cd21, reversing
    changes made to 9740c5fe220a011240dcd010159fd0fbb5b6f471.

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -18,7 +18,88 @@
                         <h2 class="card-title">{{ __('customLabels.Register') }}</h2>
                         <form method="POST" action="{{ route('register') }}">
                             @csrf
-                            @include('partials._userInfoInput')
+
+                            <!--Firstname + lastname-->
+                            <div class="row my-3">
+                                <div class="col-6">
+                                    <label for="name" class="form-label">{{ __('customLabels.firstname') }}</label>
+                                    <input class="form-control" type="text" name="name" value="{{old('name')}}"
+                                           required autocomplete="name" autofocus @error('name') is-invalid @enderror>
+                                    @error('name')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="col-6">
+                                    <label for="lname" class="form-label">{{ __('customLabels.lastname') }}</label>
+                                    <input class="form-control" type="text" name="lname" value="{{old('lname')}}"
+                                           required autocomplete="lname" @error('lname') is-invalid @enderror>
+                                    @error('lname')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <!--email + phone-->
+                            <div class="row my-3">
+                                <div class="col-6">
+                                    <label for="email" class="form-label">{{ __('customLabels.email') }}</label>
+                                    <input class="form-control" type="email" name="email" value="{{old('email')}}"
+                                           required autocomplete="email" @error('email') is-invalid @enderror>
+                                    @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="col-6">
+                                    <label for="phone" class="form-label">{{ __('customLabels.phone') }}</label>
+                                    <input class="form-control" type="phone" name="phone" value="{{old('phone')}}"
+                                           required autocomplete="phone" @error('phone') is-invalid @enderror>
+                                    @error('phone')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                            </div>
+                            <!--Birthday + gender-->
+                            <div class="row my-3">
+                                <div class="col-6">
+                                    <label for="birthday" class="form-label">{{ __('customLabels.birthday') }}</label>
+                                    <input class="form-control" type="date" name="birthday" value="{{old('birthday')}}"
+                                           required autocomplete="birthday" @error('birthday') is-invalid @enderror>
+                                    @error('birthday')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                </div>
+                                <div class="col-6">
+                                    <label for="gender" class="form-label">{{ __('customLabels.gender') }}</label>
+                                    <select class="form-select" name="gender"
+                                            required @error('gender') is-invalid @enderror>
+                                        <option selected>{{ __('customLabels.choose')}}</option>
+                                        <option value="male">{{__('customLabels.male')}}</option>
+                                        <option value="female">{{__('customLabels.female')}}</option>
+                                        <option value="other">{{__('customLabels.other')}}</option>
+
+                                        {{-- Implement when gender table exists (remember controller)
+                                        @foreach($genders as $id => $gender)
+                                            <option value="{{$id}}" {{old('gender') == $id? 'selected' : ''}}>{{ trans('customLabels.' . $gender->name)}}</option>
+                                        @endforeach --}}
+
+                                        @error('gender')
+                                        <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                    @enderror
+                                    </select>
+                                </div>
+                            </div>
+
                             <!--Passwords-->
                             <div class="row my-3">
                                 <div class="col-6">
