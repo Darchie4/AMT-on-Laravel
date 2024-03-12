1892f2cecc4d3cfe280b7e0e297c8a8e97d9eac1
Katrine Birk <katrinebirk2@live.dk>
2024-03-12 08:56:05 +0100

    added unmerged changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -82,17 +82,31 @@
     </nav>
 
     <main class="py-4">
-        @if(session('success'))
-            <div class="alert alert-success">
-                {{ session('success') }}
-            </div>
-        @endif
+        <div class="container">
+            @if ($errors->any())
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li>{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+            @if(session('success'))
+                <div class="alert alert-success alert-dismissible fade show" role="alert">
+                    {{ session('success') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
 
-        @if(session('error'))
-            <div class="alert alert-danger">
-                {{ session('error') }}
-            </div>
-        @endif
+            @if(session('error'))
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">
+                    {{ session('error') }}
+                    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+            @endif
+        </div>
         @yield('content')
     </main>
 </div>
---
+++ resources/views/users/admin/index.blade.php
@@ -0,0 +1,44 @@
+@extends('layouts.admin')
+
+@section('admin_content')
+    <div class="container">
+        <h2>{{__('customLabels.users')}}</h2>
+        <div class="d-grid d-md-flex justify-content-md-end"> <br>
+            <a class="btn btn-outline-primary mb-2" role="button" href="{{route('admin.users.create')}}">{{__('customLabels.create')}}</a>
+        </div>
+        <div class="row justify-content-center">
+            <div>
+                <table class="table table-bordered border-primary">
+                    <thead class="table-primary">
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">{{__('customLabels.email')}}</th>
+                        <th scope="col">{{__('customLabels.firstname')}}</th>
+                        <th scope="col">{{__('customLabels.lastname')}}</th>
+                        <th scope="col">{{__('customLabels.manage')}}</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    @foreach($users as $user)
+                        <tr>
+                            <td>{{$user->id}}</td>
+                            <td><a href="{{route('admin.users.show',$user->id)}}">{{$user->email}}</a></td>
+                            <td>{{$user->name}}</td>
+                            <td>{{$user->lname}}</td>
+                            <td>
+                                <a role="button" class="btn btn-outline-primary" href="{{route('admin.users.edit', $user->id)}}">{{__('customLabels.edit')}}</a>
+                                <form class="d-inline-flex" method="post" action="{{route('admin.users.destroy',$user->id)}}"
+                                      onsubmit="return confirm('{{__('customLabels.confirm')}}')">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger">{{__('customLabels.delete')}}</button>
+                                </form>
+                            </td>
+                        </tr>
+                    </tbody>
+                    @endforeach
+                </table>
+            </div>
+        </div>
+    </div>
+@endsection
